b2753a83fb40395d5aa790434d8afcca58a2ef0c
Jpst01 <jayeshwar.tanwar01@gmail.com>
2026-01-05 17:08:14 +0530

    Modified ros_gz_bridge.yaml, load_ros2_controllers.launvh.py and other files

--- mobile_robot_bringup/launch/load_ros2_controllers.launch.py
+++ mobile_robot_bringup/launch/load_ros2_controllers.launch.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python3
 
-from xml.dom import Node
 from launch import LaunchDescription
-from launch_ros.actions import LoadComposableNodes
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
+
     joint_state_broadcaster_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=["joint_state_broadcaster",
-                   "--controller-manager",
-                   "/controller_manager"],
-        output="screen"
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+        output="screen",
     )
 
     diff_drive_controller_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=["diff_drive_controller",
-                   "--controller-manager",
-                   "/controller_manager"],
-        output="screen"
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_drive_controller"],
+        output="screen",
     )
 
     return LaunchDescription([
         joint_state_broadcaster_spawner,
-        diff_drive_controller_spawner
+        diff_drive_controller_spawner,
     ])
